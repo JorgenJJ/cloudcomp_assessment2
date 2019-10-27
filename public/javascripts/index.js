d289edafd7791852fcf9bc7e9460ebceba466b1b
JorgenJJ <jorgen.jarnes@gmail.com>
2019-10-27 13:55:25 +1000

    Added comments to index.js

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,41 +1,32 @@
 let root = {"children": []};
-let index = 0,
-    format = d3.format(",d"),
-    totalTweets = 50;
+let index = 0,  // Used to select specific bubbles from the charts
+    totalTweets = 50; // The amount of tweets fetched using the API
 
+// Size of the bubble chart
 const width = 600,
       height = 400;
 
-let diameter = 300;
-let relevancyTotal = 0;
+let relevancyTotal = 0; // Used to calculate the size of bubbles comapred to all bubbles
 
-// let svg = { min: 10, max: 80 },
-//       circles = { min: 10, max: 80 },
-//       circleSize = { min: 10, max: 80 };
-
-var color = d3.scale.ordinal()
-    .domain(["Sqoop", "Pig", "Apache", "a", "b", "c", "d", "e", "f", "g"])
-    .range(["steelblue", "pink", "lightgreen", "violet", "orangered", "green", "orange", "skyblue", "gray", "aqua"]);
-
+// Used by D3, template for bubbles
 var bubble = d3.layout.pack()
     .sort(null)
     .size([height, height])
     .padding(5);
 
+// Bubble chart object
 let svg = d3.select("#bubble-chart")
   .append("svg")
     .attr("width", width)
     .attr("height", height)
     .attr("class", "bubble");
 
+// Updates or sets the size, name and colour of all bubbles
 function updateBubbles(root) {
-  if (root.children.length > 1) {
-    for(let i = 0; i < root.children.length; i++) {
-      root.children[i].size = Math.round((root.children[i].relevancy / relevancyTotal) * 10000) / 100;
+  if (root.children.length > 1) { // If there are objects in the array
+    for(let i = 0; i < root.children.length; i++) { // Loop through all objects
+      root.children[i].size = Math.round((root.children[i].relevancy / relevancyTotal) * 10000) / 100;  // Calculate size with 2 decimals
       console.log(root.children[i].name + ": " + root.children[i].score);
-      //console.log(root.children[i].name + ": " + Math.round((root.children[i].relevancy / relevancyTotal) * 10000) / 100 + "%");
-
-
     }
   }
 
@@ -56,19 +47,24 @@ function updateBubbles(root) {
   // re-use enter selection for circles
   nodeEnter
     .append("circle")
-    .attr("r", function (d) {return d.r;})
+    .attr("r", function (d) {
+      if (d.r < 10) { // If radius is smalled than 10
+        return 10;  // Set radius to 10
+      }
+      else return d.r;
+    })
     .attr("id", function (d) {return "circle_" + d.index})
     .attr("onclick", function (d) {return "selectCircle(" + d.index + ")"})
     .style("fill", function (d, i) {
       console.log(d.className + ": " + d.score);
-      if (d.score < 0) {
+      if (d.score < 0) {  // If score is negative
         let n = 255 + ((d.score / 50) * 255);
-        if (n > 200) n = 200;
+        if (n > 200) n = 200; // Prevent bubble from turning white
         return "rgb(255, " + n + ", " + n + ")";
       }
-      else {
+      else {  // If score is positive
         let n = 255 - ((d.score / 50) * 255);
-        if (n > 200) n = 200;
+        if (n > 200) n = 200; // Prevent bubble from turning white
         return "rgb(" + n + ", 255, " + n + ")";
       }
     })
@@ -80,15 +76,17 @@ function updateBubbles(root) {
       return d.className;
   });
 
+  // selection for circless
   node.select("circle")
     .transition().duration(1000)
     .attr("r", function (d) {
-      if (d.r < 10) {
-        return 10;
+      if (d.r < 10) { // If radius is less than 10
+        return 10;  // Set radius to 10
       }
       else return d.r;
   });
 
+  // re-use enter selection for text
   nodeEnter
     .append("text")
     .attr("dy", ".3px")
@@ -127,10 +125,10 @@ function updateBubbles(root) {
       children: classes
     };
   }
-
-    //d3.select(self.frameElement).style("height", diameter + "px");
 }
 
+// Creates a single bubble
+// *NO LONGER IN USE*
 function createCircle() {
   let name = document.getElementById("inp_name").value;
   let size = document.getElementById("inp_number").value;
@@ -145,12 +143,14 @@ function createCircle() {
   updateBubbles(root);
 }
 
+// Create a single circle
 function createCircle(name, array, size, score, relevancy, seconds) {
   root.children.push({"index": index++, "name": name, "array": array, "size": Number(size), "score": Number(score), "relevancy": Number(relevancy), "seconds": Number(seconds) });
-  document.getElementById("inp_search").value = "";
+  document.getElementById("inp_search").value = ""; // Clear input field
   updateBubbles(root);
 }
 
+// Select a single circle
 function selectCircle(circle) {
   let body = document.getElementById("information");
   if (body.childNodes.length != 0) body.removeChild(body.childNodes[0]);
@@ -159,8 +159,8 @@ function selectCircle(circle) {
   let circleName = circleElement.nextSibling.nextSibling.innerHTML;
 
   let c;
-  for (let i = 0; i < root.children.length; i++) {
-    if (root.children[i].name == circleName) c = root.children[i];
+  for (let i = 0; i < root.children.length; i++) {  // Loop through all objects in the array
+    if (root.children[i].name == circleName) c = root.children[i]; // Store the one matching the name from the circle clicked on
   }
 
   let div = document.createElement("div");
@@ -169,16 +169,14 @@ function selectCircle(circle) {
   let title = document.createElement("h1");
   title.innerHTML = circleName;
 
-  let pm = (Math.round(1 / (c.seconds / totalTweets) * 100) / 100);
+  let pm = (Math.round(1 / (c.seconds / totalTweets) * 100) / 100); // Calculate tweets per minute
   let tpm = document.createElement("h3");
-  if (c.seconds == 0) tpm.innerHTML = "More than " + totalTweets + " per second";
-  else if ( 1 / (c.seconds / totalTweets) > (1 / 60)) tpm.innerHTML = "Tweets per second: " + pm;
-  else tpm.innerHTML = "Tweets per second: Less than one an minute";
-
-  //tpm.innerHTML = "Time between first and last tweet: " + Math.floor(c.seconds / 60) + ":" + (c.seconds % 60);
+  if (c.seconds == 0) tpm.innerHTML = "More than " + totalTweets + " per second"; // If all tweets are posted the same second
+  else if ( 1 / (c.seconds / totalTweets) > (1 / 60)) tpm.innerHTML = "Tweets per second: " + pm; // If tweets per second is a normal number
+  else tpm.innerHTML = "Tweets per second: Less than one an minute";  // If tweets are infrequent
 
   let del;
-  if (root.children.length > 1) {
+  if (root.children.length > 1) { // Only add delete button if there are more than 1 circle
     del = document.createElement("input");
     del.setAttribute("id", "btn_deleteCircle");
     del.setAttribute("type", "button");
@@ -189,9 +187,10 @@ function selectCircle(circle) {
   let tweets = document.createElement("div");
   tweets.setAttribute("class", "div_tweet_list");
 
-  for (let j = 0; j < root.children.length; j++) {
-    if (root.children[j].index == circle) {
-      for (let i = 0; i < root.children[j].array.length; i++) {
+  // Creating the tweet list
+  for (let j = 0; j < root.children.length; j++) {  // Loop through all circle objects
+    if (root.children[j].index == circle) { // If the correct object is selected
+      for (let i = 0; i < root.children[j].array.length; i++) { // Loop through all tweets
         let tweet = document.createElement("div");
         tweet.setAttribute("class", "div_tweet");
 
@@ -222,26 +221,28 @@ function selectCircle(circle) {
 
   div.append(title);
   div.append(tpm);
-  if (root.children.length > 1) div.append(del);
+  if (root.children.length > 1) div.append(del);  // Only append delete button if more than 1 bubble
   div.append(tweets);
   document.getElementById("information").append(div);
 }
 
+// Delete a single bubble
 function deleteCircle(circle) {
   let circleElement = document.getElementById("circle_" + circle);
   let circleName = circleElement.nextSibling.nextSibling.innerHTML;
 
   let c;
-  for (let i = 0; i < root.children.length; i++) {
-    if (root.children[i].name == circleName) c = root.children[i];
+  for (let i = 0; i < root.children.length; i++) {  // Loop though all objects in the array
+    if (root.children[i].name == circleName) c = root.children[i];  // If correct object
   }
 
   let body = document.getElementById("information");
-  if (body.childNodes.length != 0) body.removeChild(body.childNodes[0]);
-  for (let i = 0; i < root.children.length; i++) {
-    if (root.children[i].index == circle) root.children.splice(i, 1);
+  if (body.childNodes.length != 0) body.removeChild(body.childNodes[0]);  // If information is displayed, remove it
+  for (let i = 0; i < root.children.length; i++) {  // Loop through all objects in the array
+    if (root.children[i].index == circle) root.children.splice(i, 1); // Remove the object that is deleted
   }
 
+  // Send a delete request to the server
   $.ajax({
     url: '/api/delete',
     method: 'post',
@@ -254,10 +255,10 @@ function deleteCircle(circle) {
       console.log("AJAX: ERROR");
     }
   });
-  // root.children.splice(circle, 1);
   updateBubbles(root);
 }
 
+// Request a new bubble based on input
 function sendRequest() {
   if (document.getElementById("inp_search").value != "") {
     $.ajax({
@@ -265,10 +266,10 @@ function sendRequest() {
       method: 'post',
       data: { query: document.getElementById("inp_search").value }
     }).done(function(res) {
-      if (res.success) {
+      if (res.success) {  // If bubble is received
         console.log("AJAX: SUCCESS");
-        relevancyTotal += res.data.relevancy;
-        createCircle(res.data.query, res.data.array, 1, res.data.score, res.data.relevancy, res.data.seconds);
+        relevancyTotal += res.data.relevancy; // Add its relevancy to the total relevancy
+        createCircle(res.data.query, res.data.array, 1, res.data.score, res.data.relevancy, res.data.seconds);  // Add the bubble
       }
       else {
         console.log("AJAX: ERROR");
@@ -282,19 +283,18 @@ function getAllCircles() {
     url: "/api/twitterAll",
     method: "get"
   }).done(function(res) {
-    if (res.success) {
+    if (res.success) {  // If all bubbles are received
       console.log("AJAX: SUCCESS");
-      console.log(res.data);
-      for (let i = 0; i < res.data.length; i++) {
-        if (res.source == "REDIS") {
+      for (let i = 0; i < res.data.length; i++) { // Loop through all received bubbles
+        if (res.source == "REDIS") {  // If collected from Redis cache
           let resultJSON = JSON.parse(res.data[i].data);
-          let name = res.data[i].id.split(":");
-          relevancyTotal += resultJSON.relevancy;
+          let name = res.data[i].id.split(":"); // Retrieve the name of the bubble
+          relevancyTotal += resultJSON.relevancy; // Add its relevancy to the total relevancy
           createCircle(name[1], resultJSON.tweetArray, 1, resultJSON.score, resultJSON.relevancy, resultJSON.seconds);
         }
-        else if (res.source == "S3") {
-          let name = res.data[i].id.split("-");
-          relevancyTotal += res.data[i].data.relevancy;
+        else if (res.source == "S3") {  // If collected from S3 Store
+          let name = res.data[i].id.split("-"); // Retrieve the name of the bubble
+          relevancyTotal += res.data[i].data.relevancy; // Add its relevancy to the total relevancy
           createCircle(name[1], res.data[i].data.tweetArray, 1, res.data[i].data.score, res.data[i].data.relevancy, res.data[i].data.seconds);
         }
       }
